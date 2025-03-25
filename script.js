58d31ed3a2d73ce4c503c13f8e57c4f696e779ad
NgheSiGia <163287898+NgheSiGia@users.noreply.github.com>
2025-03-25 21:49:33 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+    if (play) {
+        noCount++;
+        const imageIndex = Math.min(noCount, MAX_IMAGES);
+        changeImage(imageIndex);
+        resizeYesButton();
+        updateNoButtonText();
+        if (noCount === MAX_IMAGES) {
+            play = false;
+        }
+    }
+});
+
+function handleYesClick() {
+    titleElement.innerHTML = "Anh hứa hongg làm em tức nữa đouuu :3";
+    buttonsContainer.classList.add("hidden");
+    changeImage("yes");
+}
+
+function resizeYesButton() {
+    const computedStyle = window.getComputedStyle(yesButton);
+    const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+    const newFontSize = fontSize * 1.6;
+
+    yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+    const messages = [
+        "Không Bao Giờ",
+        "Anh biết lỗi rồi mò",
+        "Tha lỗi choo anhh đi mờ :((",
+        "Anh sai rồi , anh đáng trách nứa",
+        "Đừng giận anh nữa mà anh xin lỗi",
+        "Anh lo cho em lắm đó 😘😘😘😘",
+    ];
+
+    const messageIndex = Math.min(noCount, messages.length - 1);
+    return messages[messageIndex];
+}
+
+function changeImage(image) {
+    catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+    noButton.innerHTML = generateMessage(noCount);
+}
